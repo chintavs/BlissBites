a492331f962a1031e20a081d978ec206e424668d
ajwhite00 <aaronjwhite00@gmail.com>
2024-02-15 16:22:30 -0500

    Initial Project

---
+++ src/test/java/com/myplantdiary/blissbites/BlissbitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myplantdiary.blissbites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlissbitesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
