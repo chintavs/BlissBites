3dc04040ec144c3d3dfd6df9376179d08faa9d0c
smith8dk <smith8dk@mail.uc.edu>
2024-03-04 15:53:12 -0500

    Placeholder contact/store page
    Added basic UI elements to the two pages. No navigation yet.

--- src/main/java/com/myplantdiary/blissbites/BlissbitesApplication.java
+++ src/main/java/com/myplantdiary/blissbites/BlissbitesApplication.java
@@ -2,6 +2,9 @@ package com.myplantdiary.blissbites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BlissbitesApplication {
@@ -10,4 +13,18 @@ public class BlissbitesApplication {
         SpringApplication.run(BlissbitesApplication.class, args);
     }
 
+    @Controller
+    public static class HomeController {
+        
+        @GetMapping("/")
+        public String home(Model model) {
+            model.addAttribute("message", "Welcome to Bliss Bites!");
+            return "index"; // returning index.html
+        }
+
+        @GetMapping("/store")
+        public String store(Model model) {
+            return "store"; // returning store.html
+        }
+    }
 }
