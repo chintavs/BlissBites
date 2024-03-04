67474dd1e3f340d1408af716d03695b49ef89fb7
ajwhite00 <aaronjwhite00@gmail.com>
2024-03-04 16:06:38 -0500

    Merge UI Changes

--- src/main/java/com/myplantdiary/blissbites/BlissbitesApplication.java
+++ src/main/java/com/myplantdiary/blissbites/BlissbitesApplication.java
@@ -2,9 +2,6 @@ package com.myplantdiary.blissbites;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BlissbitesApplication {
@@ -13,18 +10,4 @@ public class BlissbitesApplication {
         SpringApplication.run(BlissbitesApplication.class, args);
     }
 
-    @Controller
-    public static class HomeController {
-        
-        @GetMapping("/")
-        public String home(Model model) {
-            model.addAttribute("message", "Welcome to Bliss Bites!");
-            return "index"; // returning index.html
-        }
-
-        @GetMapping("/store")
-        public String store(Model model) {
-            return "store"; // returning store.html
-        }
-    }
 }
