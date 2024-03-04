8e13ac716958a3f1049b8e0386dd770e37a78942
Vishvak C <chintavs@mail.uc.edu>
2024-03-04 16:55:23 -0500

    Added Test for adding Item to shopping cart

--- src/test/java/com/myplantdiary/blissbites/BlissbitesApplicationTests.java
+++ src/test/java/com/myplantdiary/blissbites/BlissbitesApplicationTests.java
@@ -1,13 +1,46 @@
 package com.myplantdiary.blissbites;
 
+import com.myplantdiary.blissbites.dto.Desert;
+import com.myplantdiary.blissbites.dto.ShoppingCartItem;
+import com.myplantdiary.blissbites.service.implementation.ShoppingCartService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class BlissbitesApplicationTests {
+    
+    private ShoppingCartService shoppingCartService;
+    private Desert dessert = new Desert();
+
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    void addDessertToShoppingCart() {
+        givenDessertDataAreAvailable();
+        whenUserAddsDessertToShoppingCart();
+        thenDessertIsInCart();
+    }
+
+    private void givenDessertDataAreAvailable() {
+        shoppingCartService = new ShoppingCartService();
+    }
+
+    private void whenUserAddsDessertToShoppingCart() {
+        dessert.setId(1);
+        dessert.setName("Chocolate Cake");
+        dessert.setType("Cake");
+        dessert.setDescription("Delicious Chocolate Cake made with Hershey's");
+        dessert.setCost(9.99);
+        shoppingCartService.addToShoppingCart(dessert, 1);
+    }
+
+    private void thenDessertIsInCart() {
+        ShoppingCartItem cartItem = shoppingCartService.getShoppingCartItem(dessert);
+        assertEquals(dessert, cartItem.getDesert());
+    }
 }
